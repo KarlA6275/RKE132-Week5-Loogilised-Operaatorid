a76f8a29df6a4aad8cf4cc9a00c71cb4b0ce8742
KarlA <Karl.adamson@tktk.ee>
2024-03-15 11:42:38 +0200

    add wee5 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,21 @@
+﻿//Math >= 90; Biology >= 90; Chemistry >= 90;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Kirjutage oma matemaatika tulemus:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Kirjutage oma bioloogia tulemus:");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Kirjutage oma keemia tulemus:");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Õnnitleme sisse saamise puhul.");
+}
+else
+{
+    Console.WriteLine("Teie taotlus on tagasi lükatud.");
+}
---
+++ Week5LogicalOperators/Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//AND - &&
+
+//true && true &&  true --> true
+//ture && false --> false
+//false && treu --> false
+//fasle && false --> false
+
+
+//OR - || (pipes)
+
+//true || true --> true
+//true || false --> true
+//false || true --> true
+//false || false --> false
